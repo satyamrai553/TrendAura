bff6346b4cdf0f2ea5e951e7d66b7d167d51f42f
Satyam Rai <satyamrai553@gmail.com>
2025-02-11 01:13:41 +0530

    try fixing url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,13 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
+  server:{
+    proxy:{
+      '/api': 'https://trend-aura-backend-mu.vercel.app:8000/'
+    }
+  },
   plugins: [react(),
     tailwindcss(),
   ],
+
 })
