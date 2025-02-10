2995b5d052631b0bcf2c698cd48f3b715fdbb235
Satyam Rai <satyamrai553@gmail.com>
2025-02-11 01:22:27 +0530

    api change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api': 'https://trend-aura-backend-mu.vercel.app:8000/'
-    }
-  },
   plugins: [react(),
     tailwindcss(),
   ],
